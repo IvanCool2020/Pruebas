fb997c834d3740ea47acb3d34c0d7954cb9449c3
IvanCool2020 <vanlopez@Tecno968.onmicrosoft.com>
2023-09-23 12:00:34 -0300

    Mensaje del commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+app = Flask(__name__)
+
+def create_plot():
+    tips = sns.load_dataset("tips")
+    sns_plot = sns.relplot(x="total_bill", y="tip", data=tips)
+    sns_plot.savefig('static/plot.png')
+
+@app.route('/')
+def home():
+    message="Hola a todos"
+    return render_template('index.html',message=message)
+
+if __name__ == '__main__':
+    create_plot()
+    app.run(debug=True)
